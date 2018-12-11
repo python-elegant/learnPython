681ad894bb041b254c73534511d845a3c361b28d
hcqin <hcqin@hotmail.com>
2018-12-11 15:33:21 +0800

    The first day

---
+++ pythonprogram.py
@@ -0,0 +1,20 @@
+i = 5
+print(i)
+i += 1
+print(i)
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+
+s = 'This is a string. \
+This continues the string.'
+print(s)
+
+s = 'This is a string. ' \
+    'This continues the string.'
+print(s)
+
+x = \
+5
+print(x)
---
+++ str_format.py
@@ -0,0 +1,28 @@
+age = 20
+name = 'Swaroop'
+
+print('{0} was {1} years old when wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+
+
+print('{0:.3f}'.format(1.0/3))
+print('{0:_^11}'.format('hello'))
+print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+
+print('a')
+print('b')
+
+print('a', end='\n')
+print('b', end='\n')
+
+print('a', end=' ')
+print('b', end='\n')
+
+print("This is the first sentence. This is the second sentence.")
+
+print("This is the first sentence."
+       " This is the second sentence.")
+
+print("This is the first sentence.\
+ This is the second sentence.")
